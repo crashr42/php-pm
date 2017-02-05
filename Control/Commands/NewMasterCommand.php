1102d312cc67a481b77bc7f65d26deb9c4227615
Nikita Koshkin <nikita.kem@gmail.com>
2017-02-05 13:42:35 +0700

    fix fail restart if any worker killed in old master process

--- Control/Commands/NewMasterCommand.php
+++ Control/Commands/NewMasterCommand.php
@@ -42,44 +42,59 @@ class NewMasterCommand extends ControlCommand
 
     private function gracefulShutdown(Bus $bus, ProcessManager $manager, Worker $worker, $workers)
     {
+        $bus->send(LogCommand::build(sprintf('Workers to restart %d', count($workers))));
+
         $worker->setStatus(Worker::STATUS_SHUTDOWN);
+
+        if (!$worker->getConnection()->isWritable()) {
+            $manager->getLogger()->warning("Worker at port {$worker->getPort()} already shutdown!");
+
+            $this->onWorkerShutdown($bus, $manager, $worker, $workers);
+
+            return;
+        }
 
         /** @var Connection $connection */
         $worker->getConnection()->on('close', function () use ($bus, $manager, $worker, $workers) {
-            if ($bus->isDie()) {
-                $manager->shutdownLock = false;
-
-                $manager->getLogger()->error('New master connection is die. Revert current master state.');
-
-                $bus->end();
-
-                return;
-            }
-
-            $manager->workersCollection()->removeWorker($worker);
-
-            $bus->send(NewWorkerCommand::build($worker->getPort()));
-
-            $message = sprintf('Shutdown http://%s:%s', $worker->getHost(), $worker->getPort());
-            $manager->getLogger()->info($message);
-            $bus->send(LogCommand::build($message));
-
-            if (count($workers) > 0) {
-                $this->gracefulShutdown($bus, $manager, array_shift($workers), $workers);
-            } else {
-                $bus->send(LogCommand::build('Last worker shutdown.'));
-                $bus->send(PrepareMasterCommand::build());
-
-                $manager->getLoop()->addTimer(2, function () use ($bus, $manager) {
-                    $manager->getLogger()->info(sprintf('Cluster migrate to new master at pid %s.', $this->data['pid']));
-
-                    $manager->getLoop()->stop();
-                });
-            }
+            $this->onWorkerShutdown($bus, $manager, $worker, $workers);
         });
         $bus->send(LogCommand::build(sprintf("Try shutdown http://%s:%s\n", $worker->getHost(), $worker->getPort())));
 
         $worker->getConnection()->write(ShutdownCommand::build());
+    }
+
+    private function onWorkerShutdown(Bus $bus, ProcessManager $manager, Worker $worker, $workers)
+    {
+        if ($bus->isDie()) {
+            $manager->shutdownLock = false;
+
+            $manager->getLogger()->error('New master connection is die. Revert current master state.');
+
+            $bus->end();
+
+            return;
+        }
+
+        $manager->workersCollection()->removeWorker($worker);
+
+        $bus->send(NewWorkerCommand::build($worker->getPort()));
+
+        $message = sprintf('Shutdown http://%s:%s', $worker->getHost(), $worker->getPort());
+        $manager->getLogger()->info($message);
+        $bus->send(LogCommand::build($message));
+
+        if (count($workers) > 0) {
+            $this->gracefulShutdown($bus, $manager, array_shift($workers), $workers);
+        } else {
+            $bus->send(LogCommand::build('Last worker shutdown.'));
+            $bus->send(PrepareMasterCommand::build());
+
+            $manager->getLoop()->addTimer(2, function () use ($bus, $manager) {
+                $manager->getLogger()->info(sprintf('Cluster migrate to new master at pid %s.', $this->data['pid']));
+
+                $manager->getLoop()->stop();
+            });
+        }
     }
 
     public function serialize()
