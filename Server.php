f3fc3dce227ef5de23b4d34982cf2894c3e282b7
Nikita Koshkin <nikita.kem@gmail.com>
2017-02-07 12:18:02 +0700

    fix error message

--- Server.php
+++ Server.php
@@ -56,8 +56,8 @@ class Server extends EventEmitter implements ServerInterface
                     $conn->emit('resume');
                 });
             });
-            $parser->on('error', function ($exception) use ($conn) {
-                $conn->end("HTTP/1.1 500 Internal server error\nContent-Type: text/plain\n\n".$exception->getMessage());
+            $parser->on('error', function () use ($conn) {
+                $conn->end("HTTP/1.1 400 Bad Request\nContent-Type: text/plain\n\n");
             });
 
             $conn->on('data', [$parser, 'feed']);
